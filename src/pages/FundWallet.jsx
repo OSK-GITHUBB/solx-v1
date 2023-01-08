fcea1f4a13e90330d5b28af8713b186fe1f658aa
Mehtap Celik <mehtapcelik284@gmail.com>
2023-01-08 22:35:09 +0300

    prettier

--- src/pages/FundWallet.jsx
+++ src/pages/FundWallet.jsx
@@ -1,23 +1,13 @@
 const FundWallet = () => {
     return (
         <div>
-            <p>
-              Select a way to fund your account:
-            </p>
-            <p>
-                1- Google Pay
-            </p>
-            <p>
-                2- Apple Pay
-            </p>
-            <p>
-                3- Microsoft Pay
-            </p>
-            <p>
-                4- Bank IBAN Transfer
-            </p>
+            <p>Select a way to fund your account:</p>
+            <p>1- Google Pay</p>
+            <p>2- Apple Pay</p>
+            <p>3- Microsoft Pay</p>
+            <p>4- Bank IBAN Transfer</p>
         </div>
-      )
+    )
 }
 
 export default FundWallet
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -5,10 +5,9 @@ import * as Web3 from '@solana/web3.js'
 import { saveAs } from 'file-saver';
 
 const MyPage = () => {
-    const navigate = useNavigate()
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || false);
-    const location = useLocation()
-
+  const navigate = useNavigate()
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || false);
+  const location = useLocation()
 
   //These may be unsafe for wallet creation practices? Research more.  
   const [publicKey, setPublicKey] = useState('');
@@ -19,13 +18,11 @@ const MyPage = () => {
 
     //log & set public key
     console.log(newKeypair.publicKey.toString())
-    setPublicKey(newKeypair.publicKey.toString())    
+    setPublicKey(newKeypair.publicKey.toString())
 
     //log & set secret key
     console.log(newKeypair.secretKey.toString())
     setSecretKey(newKeypair.secretKey.toString())
-
-
   }
 
   const createWalletHandler = () => {
@@ -33,55 +30,59 @@ const MyPage = () => {
   }
 
   async function secretDownloadHandler() {
-    
     // Create a Blob with the secret key as the content and a .txt file type
-    const secretKeyBlob = new Blob(["PublicAddress: ", publicKey.toString(), " Key: ", secretKey.toString()], {type: 'text/plain;charset=utf-8'});    
+    const secretKeyBlob = new Blob(["PublicAddress: ", publicKey.toString(), " Key: ", secretKey.toString()], { type: 'text/plain;charset=utf-8' });
 
     // Create a file name for the secret key file
     const secretKeyFileName = 'secret-key.txt';
-    
+
     // Prompt the user to download the secret key file
-    saveAs(secretKeyBlob, secretKeyFileName); 
+    saveAs(secretKeyBlob, secretKeyFileName);
   }
 
-    const logoutHandle = useCallback(async () => {
-        const auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(
-            auth2.disconnect().then(console.log('LOGOUT SUCCESSFUL')),
-        )
-        localStorage.removeItem("user")
-        setUser(false)
-        navigate(location?.state?.return_url || "/", {
-            replace: true
-        })
-    }, [])
+  const logoutHandle = useCallback(async () => {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(
+      auth2.disconnect().then(console.log('LOGOUT SUCCESSFUL')),
+    )
+    localStorage.removeItem("user")
+    setUser(false)
+    navigate(location?.state?.return_url || "/", {
+      replace: true
+    })
+  }, [])
 
-    const onPress = () => {
-        navigate("/fund-wallet")
-    }
+  const onPress = () => {
+    navigate("/fund-wallet")
+  }
 
-    return <div className="flexDirectionColumn">
-        <h2 className="white">Hoş geldin {user?.name}</h2>
-        <button style={{ marginLeft: 24 }} onClick={logoutHandle}>Log out</button>
-        <div>
+  return <div className="flexDirectionColumn">
+    <h2 className="white">Hoş geldin {user?.name}</h2>
+    <button style={{ position: "absolute", right: 24, top: 24 }} onClick={logoutHandle}>Log out</button>
+    <div className="center">
+      <body>
         <p>
           Click to create a wallet for your account:
         </p>
-        <button onClick={createWalletHandler} >Create a new wallet</button>
-
-        <p></p>
-
-        {publicKey !== '' && (
-          <>
-            <p>Wallet Created:</p>
-            <p>Public Key: {publicKey}</p>
-            <p>Private Key: <button onClick={secretDownloadHandler} >Click to download</button> <button>Click to upload to cloud--Coming Soon</button> </p>
-              <button onClick={onPress}> Next {"> > >"} </button>            
-          </>
-        )}
+        <div className="center">
+          <button onClick={createWalletHandler} >Create a new wallet</button>
+        </div>
+        <br />
+        {
+          publicKey !== '' && (
+            <>
+              <p>Wallet Created:</p>
+              <p>Public Key: {publicKey}</p>
+              <p>Private Key: <button onClick={secretDownloadHandler} >Click to download</button> <button>Click to upload to cloud--Coming Soon</button> </p>
+              <div className="center">
+                <button onClick={onPress}> Next {"> > >"} </button>
+              </div>
+            </>
+          )
+        }
+      </body>
     </div>
-    
-    </div>
+  </div>
 }
 
 export default MyPage;
