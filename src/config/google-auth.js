a78cec223e3bfaf5b3d7004e1531f0f797168d1f
OSK <48172267+OSK-GITHUBB@users.noreply.github.com>
2023-01-18 15:29:37 +0300

    google oauth updates

--- src/config/google-auth.js
+++ src/config/google-auth.js
@@ -1,1 +1,2 @@
-export const clientId = "1016788651463-i37su24ign7m5lsjircu6h9lo0bi4al2.apps.googleusercontent.com";
+// export const clientId = "1016788651463-i37su24ign7m5lsjircu6h9lo0bi4al2.apps.googleusercontent.com";
+export const clientId = "824538867486-lfbvfo932qvsrpo4gpvmcn5iglqv48o3.apps.googleusercontent.com";
--- src/config/google-auth.tsx
+++ src/config/google-auth.tsx
@@ -1,1 +1,3 @@
-export const clientId: string = "1016788651463-i37su24ign7m5lsjircu6h9lo0bi4al2.apps.googleusercontent.com";
+// export const clientId: string = "1016788651463-i37su24ign7m5lsjircu6h9lo0bi4al2.apps.googleusercontent.com";
+
+export const clientId: string = "824538867486-lfbvfo932qvsrpo4gpvmcn5iglqv48o3.apps.googleusercontent.com";
